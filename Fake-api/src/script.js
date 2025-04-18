76e510c8bf107a9a86e57c10b5e7f2bd1dabec12
YaAlan1 <chill.alanus@gmail.com>
2025-04-18 21:07:37 +0500

    Add files via upload

---
+++ Fake-api/src/script.js
@@ -0,0 +1,13 @@
+
+let click = document.querySelectorAll('.card')
+
+click.onclick = () => {
+    window.location = '/Fake-api/src/Product/'
+}
+
+
+const log = document.querySelector('.log'); 
+
+log.onclick = () => {
+    window.location = '/';
+};
